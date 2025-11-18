c96145797b0ceedb54d7942f5c1b1f5ec65f8b7d
yrs <1531331257@qq.com>
2025-11-18 15:00:05 +0800

    增加文件暴露路径，修改levedb版本

--- Package.swift
+++ Package.swift
@@ -3,9 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "DinCore",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DinCore",
             targets: ["DinCore"]
@@ -15,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/yrs211/DinSupport-SPM.git", exact: "1.0.2"),
         .package(url: "https://github.com/yrs211/AlicloudHttpDNS-SPM.git", exact:"1.0.1"),
-        .package(url: "https://github.com/yrs211/Objective-LevelDB.git", exact:"1.0.2"),
+        .package(url: "https://github.com/yrs211/Objective-LevelDB.git", exact:"1.0.4"),
         .package(url: "https://github.com/yrs211/Snappy-ObjC.git", exact:"1.0.1"),
         .package(url: "https://github.com/Moya/Moya.git", exact:"15.0.0"),
         .package(url: "https://github.com/alibaba/HandyJSON.git", exact:"5.0.2"),
@@ -24,8 +23,6 @@ let package = Package(
         .package(url: "https://github.com/daltoniam/Starscream.git", exact:"3.1.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DinCore",
             dependencies: [
@@ -40,7 +37,9 @@ let package = Package(
                          .product(name: "Starscream", package: "Starscream"),
                          .product(name: "RxSwift", package: "RxSwift"),       // 对应 pod 'RxSwift'
                          .product(name: "RxCocoa", package: "RxSwift"),       // 对应 pod 'RxCocoa'（同一仓库产物）
-                     ]
+                     ],
+            path: "DinCore/DinCore/Sources",//暴露文文件夹路径
+            sources: ["."],
         ),
     ]
 )
